981ec67346973458d7f13388af2c64bea855a9c2
Benjamin Cecutti <benjamin.cecutti@dcmail.ca>
2023-12-13 17:20:42 -0500

    Inital Commit

---
+++ src/main/java/com/example/servingwebcontent/ServingWebContent100754039Application.java
@@ -0,0 +1,13 @@
+package com.example.servingwebcontent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServingWebContent100754039Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServingWebContent100754039Application.class, args);
+	}
+
+}
